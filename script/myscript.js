3cb785c1109f84e4d174c90fb72d01fa0f68f15a
decafmind <decafmind@gmail.com>
2016-05-24 15:31:48 +0800

    Good enough structure.

---
+++ script/myscript.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(function(){
+        $('#header_nav').data('size','big');
+    });
+
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 0)
+        {
+            if($('#header_nav').data('size') == 'big')
+            {
+                $('#header_nav').data('size','small');
+                $('.header').slideUp();
+            }
+        }
+        else
+        {
+        if($('#header_nav').data('size') == 'small')
+        {
+            $('#header_nav').data('size','big');
+                $('.header').slideDown();
+        } 
+    }
+    });
+});
